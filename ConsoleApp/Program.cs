384db3e45304bc2b6198b06da1c62ce6ad5a1df0
Sicker <sicker27@hotmail.com>
2022-05-25 03:44:04 +0700

    Add console app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SecurityToken;
+using Amazon.SecurityToken.Model;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var client = new AmazonSecurityTokenServiceClient())
+            {
+                var response1 = await client.GetCallerIdentityAsync(new GetCallerIdentityRequest());
+                Console.WriteLine(response1.Account);
+                var response = await client.AssumeRoleAsync(new AssumeRoleRequest { RoleArn = "arn:aws:iam::367234352884:role/iam_role", RoleSessionName = "Session1" });
+                var credentials = response.Credentials;
+
+
+                using (var sesclient = new AmazonSimpleEmailServiceClient(credentials))
+                {
+                    var sesresponse = await sesclient.SendTemplatedEmailAsync(new SendTemplatedEmailRequest
+                    {
+                        Source = "sicker27@hotmail.com",
+                        Destination = new Destination
+                        {
+                            ToAddresses = new List<string> { "sicker27@hotmail.com" }
+                        },
+                        Template = "alert_template",
+                        TemplateData = JsonSerializer.Serialize(new AlertModel { Name = "Sicker", FavoriteAnimal = "Dog" })
+                    });
+                    Console.WriteLine(sesresponse.MessageId);
+                }
+            }
+        }
+    }
+
+    public class AlertModel
+    {
+        public string Name { get; set; }
+        public string FavoriteAnimal { get; set; }
+    }
+}
---
+++ ConsoleApp/terrform/main.tf
@@ -0,0 +1,68 @@
+﻿provider "aws" {
+  region  = "ap-southeast-1"
+  profile = "default"
+}
+
+
+variable "additional_tags" {
+  default     = {}
+  description = "Additional resource tags"
+  type        = map(string)
+}
+
+resource "aws_iam_role" "alert_role" {
+  name = "iam_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect   = "Allow"
+        Principal: {
+          "AWS": "arn:aws:iam::367234352884:user/sicker_admin"
+        },
+      },
+    ]
+  })
+
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "iam_role"
+    },
+    )
+}
+
+resource "aws_iam_role_policy" "ses_policy" {
+  name = "ses_policy"
+  role = aws_iam_role.alert_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ses:SendTemplatedEmail",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_ses_email_identity" "alert_identity" {
+  email = "sicker27@hotmail.com"
+}
+
+resource "aws_ses_template" "alert_template" {
+  name    = "alert_template"
+  subject = "Test Email"
+  html    = "<h1>Hello {{Name}},</h1><p>Your favorite animal is {{FavoriteAnimal}}.</p>"
+  text    = "Hello {{Name}},\r\nYour favorite animal is {{FavoriteAnimal}}."
+}
+
+output "alert_role_arn" {
+  value = aws_iam_role.alert_role.arn
+}
